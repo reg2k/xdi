b2f2b171d55aa66664736760a4323033ee92dd18
reg2k <23437153+reg2k@users.noreply.github.com>
2018-02-15 03:20:13 +0000

    Pass on exit code

--- build.py
+++ build.py
@@ -1,10 +1,10 @@
-import os
+import os, sys
 
 # Configuration
 BUILD_DIR        = 'build'
 PROJECT_DIR      = 'src'
 PLATFORM_TOOLSET = 'v140'
-F4SE_REVISION    = 'master'
+F4SE_REVISION    = 'tags/v0.6.5'
 BUILD_TOOLS_REPO = 'https://github.com/reg2k/f4se-build-tools.git'
 
 # Fetch F4SE build tools
@@ -12,5 +12,8 @@ if not os.path.exists('{}/build-tools'.format(BUILD_DIR)):
     os.system('git clone {} {}/build-tools'.format(BUILD_TOOLS_REPO, BUILD_DIR))
 
 # Run build tools
-os.system('python {}/build-tools/build_plugin.py "{}" "{}" "{}" "{}"'
+buildOK = os.system('python {}/build-tools/build_plugin.py "{}" "{}" "{}" "{}"'
     .format(BUILD_DIR, BUILD_DIR, PROJECT_DIR, PLATFORM_TOOLSET, F4SE_REVISION))
+    
+# Report result
+sys.exit(buildOK)
